bc696092d1850e0069674c3ee83809796355eebb
Michael OBrien <obrien.michael.t@gmail.com>
2025-07-22 11:36:04 -0400

    Updates serializer after Postman tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name])
 
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  # Required for self revocation strategy
+  def self.jwt_revoked?(payload, user)
+    user.jti != payload['jti']
+  end
+
+  def self.revoke_jwt(payload, user)
+    user.update_column(:jti, SecureRandom.uuid)
+  end
+
+  # Generate jti on user creation
+  before_create :generate_jti
+
+  private
+
+  def generate_jti
+    self.jti = SecureRandom.uuid
+  end
 end
