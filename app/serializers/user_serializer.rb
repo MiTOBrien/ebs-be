b1158e31921449c672742dffa9a3af403d1a2617
Michael OBrien <obrien.michael.t@gmail.com>
2025-07-29 15:38:33 -0400

    updates user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,8 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :id, :email, :first_name, :last_name, :professional, :username, :roles,:bio, :profile_picture, :facebook, :instagram, :x
+  attributes :id, :email, :first_name, :last_name, :professional, :username, :bio, :profile_picture, :facebook, :instagram, :x
+
+  attribute :roles do |user|
+    user.user_roles.includes(:role).map(&:role)
+  end
 end
