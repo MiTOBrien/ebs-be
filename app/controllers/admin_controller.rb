b80ff621805322b96d9ba5d6304586d6295496de
Michael OBrien <obrien.michael.t@gmail.com>
2025-08-02 10:09:44 -0400

    sets up structure for admin page on frontend

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,10 @@
+class AdminController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_admin!
+
+  private
+
+  def ensure_admin!
+    redirect_to root_path unless current_user&.admin?
+  end
+end
