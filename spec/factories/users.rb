bfbd259e41d8c5852b7490779825e7f3f7ce86e3
Michael OBrien <obrien.michael.t@gmail.com>
2025-08-14 09:51:49 -0400

    creates readers controller test - tests pass

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,23 @@
 FactoryBot.define do
   factory :user do
-    username { Faker::Internet.unique.username }
+    sequence(:username) { |n| "user_#{n}" }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    email { Faker::Internet.unique.email }
+    sequence(:email) { |n| "user_#{n}@example.com" }
     password { "password123" }
     password_confirmation { "password123" }
     charges_for_services { false }
     subscription_type { "free" }
     subscription_status { "active" }
+
+    # Add associations for roles
+    transient do
+      roles { [] }
+    end
+
+    after(:create) do |user, evaluator|
+      user.roles << evaluator.roles if evaluator.roles
+    end
 
     # Add traits for different subscription types if needed
     trait :paid_monthly do
