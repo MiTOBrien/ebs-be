a3c01912ec7eb1d7d01e48c6ce7fe47eebfc2ed8
Michael OBrien <obrien.michael.t@gmail.com>
2025-09-03 15:06:42 -0400

    updates user serializer to return the users genres and sub-genres

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -5,4 +5,21 @@ class UserSerializer
   attribute :roles do |user|
     user.user_roles.includes(:role).map(&:role)
   end
+
+  attribute :genres do |user|
+    user.user_genres.includes(genre: :subgenres).map do |user_genre|
+      genre = user_genre.genre
+      {
+        id: genre.id,
+        name: genre.name,
+        subgenres: genre.subgenres.map do |sub|
+          {
+            id: sub.id,
+            name: sub.name,
+            parent_id: genre.id
+          }
+        end
+      }
+    end
+  end
 end
