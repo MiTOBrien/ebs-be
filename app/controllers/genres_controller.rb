9f0dff527efc34765659438acb778e13dd0d5fa0
Michael OBrien <obrien.michael.t@gmail.com>
2025-09-18 15:15:09 -0400

    updates genres controller for creating and updating genres

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -5,4 +5,30 @@ class GenresController < ApplicationController
     top_level_genres = Genre.where(parent_id: nil).includes(:subgenres)
     render json: top_level_genres.as_json(include: :subgenres)
   end
+
+  def create
+    genre = Genre.new(genre_params)
+
+    if genre.save
+      render json: genre, status: :created
+    else
+      render json: { errors: genre.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def update
+    genre = Genre.find(params[:id])
+
+    if genre.update(genre_params)
+      render json: genre, status: :ok
+    else
+      render json: { errors: genre.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name, :parent_id)
+  end
+
 end
