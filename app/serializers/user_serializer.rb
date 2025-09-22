126b6fe236b0ac89c71e51272c3f2cb4396e3916
Michael OBrien <obrien.michael.t@gmail.com>
2025-09-22 12:39:58 -0400

    adds disabled to return for filtering on account status

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer
   include JSONAPI::Serializer
 
-  attributes :id, :email, :first_name, :last_name, :professional, :username, :bio, :profile_picture, :charges_for_services, :professional, :facebook, :instagram, :x
+  attributes :id, :email, :first_name, :last_name, :professional, :username, :bio, :profile_picture, :charges_for_services, :professional, :facebook, :instagram, :x, :disabled
 
   attribute :roles do |user|
     user.user_roles.includes(:role).map(&:role)
