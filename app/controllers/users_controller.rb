7829e9b1f52e44b3773d264a7e098dd45ab33352
Michael OBrien <obrien.michael.t@gmail.com>
2025-10-17 12:39:38 -0400

    updates controller to handle subscribed users on the admin page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    render json: @users.as_json(include: { roles: { only: [:id, :role] } }), status: :ok
+    render json: @users.as_json(methods:[:subscribed], include: { roles: { only: [:id, :role] } }), status: :ok
   end
 
     def upload_profile_image
